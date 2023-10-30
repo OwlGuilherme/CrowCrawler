e968c7ea9408b781d904a0b2db84c58da69d86f5
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-10-29 22:34:15 -0300

    Implementada leitura de arquivo csv para coletar os links dos produtos

--- crowcrawler/crowcrawler/spiders/mercado_livre.py
+++ crowcrawler/crowcrawler/spiders/mercado_livre.py
@@ -1,9 +1,18 @@
 import scrapy
+import csv
+
+csv_file_name = "links.csv"
 
 
 class MercadoLivreSpider(scrapy.Spider):
     name = "mercado_livre"
-    start_urls = ["https://mercadolivre.com"]
+    start_urls = []
+
+    with open(csv_file_name, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for linha in csv_reader:
+            link = linha[0]
+            start_urls.append(link)
 
     def parse(self, response, **kawargs):
         pass
