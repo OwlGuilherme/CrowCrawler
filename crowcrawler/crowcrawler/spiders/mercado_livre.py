fde54726a5fc7ffc6fcc4663030d6c38b79e68dc
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-10-29 22:54:41 -0300

    Implementado código base que permite acessar o site com spyder

--- crowcrawler/crowcrawler/spiders/mercado_livre.py
+++ crowcrawler/crowcrawler/spiders/mercado_livre.py
@@ -1,12 +1,11 @@
 import scrapy
 import csv
 
-csv_file_name = "links.csv"
-
-
 class MercadoLivreSpider(scrapy.Spider):
     name = "mercado_livre"
     start_urls = []
+
+    csv_file_name = "links.csv"
 
     with open(csv_file_name, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -15,4 +14,10 @@ class MercadoLivreSpider(scrapy.Spider):
             start_urls.append(link)
 
     def parse(self, response, **kawargs):
-        pass
+        price = xpath('//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[3]/div[1]/div[1]/span[1]/span[3]//text()').get()
+        title = xpath('//*[@id="ui-pdp-main-container"]/div[1]/div/div[1]/div[2]/div[1]/div/div[2]/h1//text()').get()
+
+        yield{
+                'price' : price,
+            'title' : title
+        }
