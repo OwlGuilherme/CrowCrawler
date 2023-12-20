80b9cfee7c4f0fec9bb19d6a8a734a630afebae5
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-20 08:01:29 -0300

    Atualizado arquivo main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from utils.db_act import criar_tabela, salvar_dados, obter_dados
 from utils.webscrap import scrape_site
-from config import SITE_RULES
+from utils.config import SITE_RULES
 
 def main():
-    site_url = 'https://www.mercadolivre.com.br/produto-exemplo'
+    site_url = 'https://produto.mercadolivre.com.br/MLB-3489320777-relogio-smart-esportivo-tranya-s2-resistente-agua-3atm-_JM#polycard_client=recommendations_home_navigation-trend-recommendations&reco_backend=machinalis-homes-pdp-boos&reco_client=home_navigation-trend-recommendations&reco_item_pos=2&reco_backend_type=function&reco_id=2d7a2e3a-dd92-46da-accc-472e7945eaa5'
     site_rules = SITE_RULES.get('mercadolivre')
 
     if site_rules:
