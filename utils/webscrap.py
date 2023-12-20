dc401ead4c953150c1b370287560fdc4b2bd899e
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-20 08:02:48 -0300

    Implementadas verificações na captura do preço e nome do produto

--- utils/webscrap.py
+++ utils/webscrap.py
@@ -1,13 +1,20 @@
+# webscrap.py
 from bs4 import BeautifulSoup
 import requests
-from config import SITE_RULES
+from utils.config import SITE_RULES
 
 def scrape_site(site_url, rules):
     response = requests.get(site_url)
     html_content = response.text
 
     soup = BeautifulSoup(html_content, 'html.parser')
-    name = soup.select_one(rules.name_selector).text.strip()
-    price = soup.select_one(rules.price_selector).text.strip()
+
+    # Verifica se o seletor do nome está presente
+    name_element = soup.select_one(rules.name_selector)
+    name = name_element.text.strip() if name_element else "Nome não encontrado"
+
+    # Verifica se o seletor do preço está presente
+    price_element = soup.select_one(rules.price_selector)
+    price = price_element.get('content').strip() if price_element else "Preço não encontrado"
 
     return {'name': name, 'price': price}
