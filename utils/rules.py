bd6a4b47e8d0ff49422017b29bbfe616dcc8c3f7
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-20 08:55:45 -0300

    Adicionadas rules para o site Netshoes

--- utils/rules.py
+++ utils/rules.py
@@ -2,3 +2,8 @@ class MercadoLivreRules:
     def __init__(self):
         self.name_selector = 'h1.ui-pdp-title'
         self.price_selector = 'meta[itemprop="price"]'
+
+class Netshoes:
+    def __init__(self):
+        self.name_selector = 'h1[data-productname]'
+        self.price_selector = 'div.default-price strong'
