d240c79ac7dcc2757676e02fd44b9d730b781eb1
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-20 11:56:10 -0300

    Implementação da leitura dos links por meio de arquivo json externo

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
-from utils.db_act import criar_tabela, salvar_dados, obter_dados
+import json
+from utils.db_act import criar_tabela, salvar_dados
 from utils.webscrap import scrape_site
 from utils.config import SITE_RULES
 
 criar_tabela()
 
+def load_urls_from_json(file_path='urls.json'):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        return data.get('urls', [])
+
 def main():
-    site_url = 'https://www.amazon.com.br/Tranya-Smartwatch-Rel%C3%B3gio-inteligente-Bluetooth/dp/B0BDDHB5Q1/ref=pd_ci_mcx_mh_mcx_views_0?pd_rd_i=B0BDDHB5Q1'
-    site_rules = SITE_RULES.get('amazon')
+    # Lista de URLs a serem raspadas
+    urls = load_urls_from_json()
 
-    if site_rules:
-        data = scrape_site(site_url, site_rules)
-        print(data)
+    for site_url in urls:
+        # Obtenha as regras do site a partir do mapeamento SITE_RULES
+        site_rules_key = None
+        for key, rules in SITE_RULES.items():
+            if key in site_url:
+                site_rules_key = key
+                break
 
-        nome_produto = data.get('name', 'Nome não encontrado')
-        preco_atual = data.get('price', 'Preço não encontrado')
-        if 'R$' in preco_atual:
-            preco_atual = preco_atual.replace('R$', '').strip()
+        if site_rules_key:
+            site_rules = SITE_RULES.get(site_rules_key)
+            data = scrape_site(site_url, site_rules)
+            print(data)
 
-         # Substituir vírgulas por pontos no preço
-        preco_atual = preco_atual.replace(',', '.')
+            nome_produto = data.get('name', 'Nome não encontrado')
+            preco_atual = data.get('price', 'Preço não encontrado')
 
-        salvar_dados(nome_produto, preco_atual)
-        print("Dados salvos no banco de dados com sucesso!")
+            if 'R$' in preco_atual:
+                preco_atual = preco_atual.replace('R$', '').strip()
 
-    else:
-        print("Houve um erro e não foi possível salvar os dados.")
+            # Substituir vírgulas por pontos no preço
+            preco_atual = preco_atual.replace(',', '.')
+
+            salvar_dados(nome_produto, preco_atual)
+            print("Dados salvos no banco de dados com sucesso")
+        else:
+            print("Regras não encontradas para o site")
 
 if __name__ == "__main__":
     main()
