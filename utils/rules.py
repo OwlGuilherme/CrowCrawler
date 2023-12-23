762051f5c656384ce0515f1c9753e06fd43e9583
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-23 00:14:13 -0300

    Acrescentados sites Netshoes e Centauros nas rules

--- utils/rules.py
+++ utils/rules.py
@@ -5,10 +5,15 @@ class MercadoLivreRules:
 
 class NetshoesRules:
     def __init__(self):
-        self.name_selector = ''
-        self.price_selector = ''
+        self.name_selector = 'h1[data-productname]'
+        self.price_selector = '.default-price span strong'
 
 class AmazonRules:
     def __init__(self):
         self.name_selector = '#productTitle'
         self.price_selector = '.a-offscreen'
+
+class CentauroRules:
+    def __init__(self):
+        self.name_selector = 'h1.ui-pdp-title'
+        self.price_selector = 'span.andes-money-amount'
