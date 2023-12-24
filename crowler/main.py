32178b2be8ff42db62f4297b11ca8ac736a26ebc
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-24 20:40:49 -0300

    Implementada raspagem de dados utilizando spyder scrapy

--- crowler/main.py
+++ crowler/main.py
@@ -1,14 +1,12 @@
 import json
 from utils.db_act import criar_tabela, salvar_dados
-from utils.webscrap import scrape_site
 from utils.config import SITE_RULES
+from scrapy.crawler import CrawlerProcess
+from crowler.spiders.mercadoLivre import MercadolivreSpider
+from crowler.spiders.amazon import AmazonSpider
+
 
 criar_tabela()
-
-def load_urls_from_json(file_path='urls.json'):
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-        return data.get('urls', [])
 
 def choose_site():
     print("Escolha o site que deseja raspar:")
@@ -31,39 +29,26 @@ def choose_site():
         print("Escolha inválida. Por favor, escolha 1, 2, 3 ou 4.")
         return choose_site()
 
-def filter_urls_by_site(urls, site_key):
-    return [url for url in urls if site_key in url]
-
 def main():
     site_key = choose_site()
     site_rules = SITE_RULES.get(site_key)
 
     if site_rules:
-        # Lista de URLs a serem raspadas
-        urls = load_urls_from_json()
-        filtered_urls = filter_urls_by_site(urls, site_key)
+        process = CrawlerProcess(settings={
+            'DOWNLOAD_DELAY': 2
+        })
 
-        if not filtered_urls:
-            print(f"Não há URLs para o site {site_key}. Encerrando.")
-            return
+        if site_key == 'amazon':
+            process.crawl(AmazonSpider, rules=site_rules)
+        elif site_key == 'mercadolivre':
+            process.crawl(MercadolivreSpider, rules=site_rules)
 
-        for site_url in filtered_urls:
-            data = scrape_site(site_url, site_rules)
-            print(data)
+        process.start()
 
-            nome_produto = data.get('name', 'Nome não encontrado')
-            preco_atual = data.get('price', 'Preço não encontrado')
-
-            if 'R$' in preco_atual:
-                preco_atual = preco_atual.replace('R$', '').strip()
-
-            # Substituir vírgulas por pontos no preço
-            preco_atual = preco_atual.replace(',', '.')
-
-            salvar_dados(nome_produto, preco_atual)
-            print("Dados salvos no banco de dados com sucesso para", site_url)
+        print("Dados salvos no banco de dados com sucesso.")
     else:
         print(f"Regras não encontradas para o site: {site_key}")
 
 if __name__ == "__main__":
     main()
+
