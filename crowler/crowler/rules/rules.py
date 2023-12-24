3609771f9fe514a061833843a892ab868c3add58
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-24 13:03:22 -0300

    Atualizadas regras do mercado livre para utilizar xpath

--- crowler/crowler/rules/rules.py
+++ crowler/crowler/rules/rules.py
@@ -1,7 +1,7 @@
 class MercadoLivreRules:
     def __init__(self):
-        self.name_selector = 'h1.ui-pdp-title'
-        self.price_selector = 'meta[itemprop="price"]'
+        self.name_selector = '//*[@id="header"]/div/div[2]/h1'
+        self.price_selector = '//*[@id="price"]/div/div[1]/div[1]/span/span/meta'
 
 class NetshoesRules:
     def __init__(self):
