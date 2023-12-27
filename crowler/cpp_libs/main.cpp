beafca97bdc4c9475ac6fb184ab5afd611b544eb
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-27 17:21:05 -0300

    Implementa integração inicial para construção do módulo python

--- crowler/cpp_libs/main.cpp
+++ crowler/cpp_libs/main.cpp
@@ -1,3 +1,5 @@
+#define PY_SSIZE_T_CLEAN
+#include <python3.12/Python.h>
 #include <cstdlib>
 #include <ctime>
 #include <exception>
@@ -7,7 +9,7 @@
 #include <sstream>
 #include <vector>
 
-void criar_tabela() {
+int criar_tabela() {
   sqlite3 *db;
   int result = sqlite3_open("banco.db", &db);
 
@@ -30,9 +32,11 @@ void criar_tabela() {
   }
 
   sqlite3_close(db);
+
+  return 0;
 }
 
-void salvar_dados(const std::string &nome, const std::string &preco_atual,
+int salvar_dados(const std::string &nome, const std::string &preco_atual,
                   const std::string &site) {
 
   sqlite3 *db;
@@ -84,6 +88,7 @@ void salvar_dados(const std::string &nome, const std::string &preco_atual,
   }
 
   sqlite3_close(db);
+  return 0;
 }
 
 std::vector<std::tuple<std::string, std::string, double, std::string>>
@@ -92,7 +97,7 @@ obter_dados() {
   int result = sqlite3_open("banco.db", &db);
 
   if (result != SQLITE_OK) {
-    std::err << "Erro ao criar banco de dados: " << sqlite3_errmsg(db) << '\n';
+    std::cerr << "Erro ao criar banco de dados: " << sqlite3_errmsg(db) << '\n';
     sqlite3_close(db);
     exit(EXIT_FAILURE);
   }
@@ -103,8 +108,8 @@ obter_dados() {
   result = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
 
   if (result != SQLITE_OK) {
-    std::err << "Erro ao preparar a consulta SQL: " << sqlite3_errmsg(db)
-             << '\n';
+    std::cerr << "Erro ao preparar a consulta SQL: " << sqlite3_errmsg(db)
+              << '\n';
     sqlite3_close(db);
     exit(EXIT_FAILURE);
   }
@@ -133,4 +138,61 @@ obter_dados() {
   sqlite3_close(db);
 
   return dados;
+}
+
+static PyObject *criar_tabela(PyObject  *self, PyObject *args){
+  criar_tabela();
+  Py_RETURN_NONE;
+}
+
+static PyObject *salvar_dados(PyObject *self, PyObject *args){
+  const char *nome;
+  const char *preco_atual;
+  const char *site;
+
+  if (!PyArg_Parse(args, "sss", &nome, &preco_atual, &site)) {
+    return nullptr;
+  }
+
+  salvar_dados(nome, preco_atual, site);
+  Py_RETURN_NONE;
+}
+
+static PyObject *obter_dados(PyObject *self, PyObject *args){
+  auto dados = obter_dados();
+
+  PyObject *result = PyList_New(dados.size());
+
+  for (std::size_t i = 0; i < dados.size(); ++i) {
+    const auto &d = dados[i];
+
+    PyObject *tuple = PyTuple_Pack(4, PyUnicode_DecodeUTF8(std::get<0>(d).c_str(), -1, nullptr),
+                                    PyUnicode_DecodeUTF8(std::get<1>(d).c_str(), -1, nullptr),
+                                    PyFloat_FromDouble(std::get<2>(d)),
+                                    PyUnicode_DecodeUTF8(std::get<3>(d).c_str(), -1, nullptr));
+
+    PyList_SetItem(result, i, tuple);
+  }
+
+  return result;
+
+}
+
+static PyMethodDef db_act_methods[] = {
+  {"criar_tabela", criar_tabela, METH_NOARGS, "Cria a tabela e o banco de dados."},
+  {"salva_dados", salvar_dados, METH_VARARGS, "Salva os dados no banco de dados."},
+  {"obter_dados", obter_dados, METH_NOARGS, "Obtem os dados do banco de dados. "},
+  {NULL, NULL, 0, NULL}
+};
+
+static struct  PyModuleDef db_act_module = {
+  PyModuleDef_HEAD_INIT,
+  "db_act",
+  NULL,
+  -1,
+  db_act_methods  
+};
+
+PyMODINIT_FUNC PyInit_db_act(void){
+  return PyModule_Create(&db_act_module);
 }
