2c95946b1c278176846da5a7407dc8ef1f89db81
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-26 23:11:17 -0300

    Criadas regras para o site Declathon

--- crowler/crowler/rules/rules.py
+++ crowler/crowler/rules/rules.py
@@ -18,3 +18,8 @@ class CentauroRules:
     def __init__(self):
         self.name_selector = '//h1[@data-testid="product-title"]/text()'
         self.price_selector = '//p[@data-testid="price-current"]/text()'
+
+class DeclathonRules:
+    def __init__(self):
+        self.name_selector = '//h1[@class="desktop-body-regular-text1 text-restructure-primary mb-0"]/text()'
+        self.price_selector = '//h2[@class="desktop-heading-title5 text-restructure-primary"]/text()'
