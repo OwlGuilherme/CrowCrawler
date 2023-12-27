e3cc88ed9934aa0241db31ebb81498e2e73e4358
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-26 23:27:41 -0300

    Corrigida importação para receber o arquivo correto de regras

--- crowler/utils/config.py
+++ crowler/utils/config.py
@@ -1,9 +1,10 @@
-from utils.rules import MercadoLivreRules, NetshoesRules, AmazonRules, CentauroRules
+from crowler.rules.rules import MercadoLivreRules, NetshoesRules, AmazonRules, CentauroRules, DeclathonRules
 
 SITE_RULES = {
     'mercadolivre': MercadoLivreRules(),
     'netshoes': NetshoesRules(),
     'amazon' : AmazonRules(),
-    'centauro' : CentauroRules()
+    'centauro' : CentauroRules(),
+    'declathon' : DeclathonRules()
 }
 
