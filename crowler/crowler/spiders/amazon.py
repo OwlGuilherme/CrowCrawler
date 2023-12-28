fe678377ce92c895a09414d2c680875872a0fcb1
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-28 16:18:59 -0300

    Implementado tratamento do preço

--- crowler/crowler/spiders/amazon.py
+++ crowler/crowler/spiders/amazon.py
@@ -1,7 +1,7 @@
 import scrapy
 from crowler.rules.rules import AmazonRules
 import json
-from utils.db_act import salvar_dados_no_banco, obter_ultimo_preco
+from utils.db_act import salvar_dados, obter_ultimo_preco, preprocessar_preco
 
 class AmazonSpider(scrapy.Spider):
     name = "amazon"
@@ -18,12 +18,15 @@ class AmazonSpider(scrapy.Spider):
     rules = AmazonRules()
 
     def parse(self, response):
-        name = response.css(self.rules.name_selector + '::text').get()
+        name = response.css(self.rules.name_selector + '::text').get().strip()
         price = response.css(self.rules.price_selector + '::text').get()
+
+        price = preprocessar_preco(price)
+
 
         # Verificar mudança de preço antes de salvar
         if self.verificar_mudanca_de_preco(name, float(price), 'Amazon'):
-            salvar_dados_no_banco(name, float(price), 'Amazon')
+            salvar_dados(name, float(price), 'Amazon')
 
     def verificar_mudanca_de_preco(self, nome, novo_preco, site, margem_tolerancia=0.05):
         ultimo_preco = obter_ultimo_preco(nome, site)
