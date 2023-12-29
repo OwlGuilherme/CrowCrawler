9a6c22685785fab90418b21397df95afd9c277f0
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-29 01:07:27 -0300

    Corrigido caminha para arquivo de links e adicionada limpeza da tela

--- crowler/utils/json_act.py
+++ crowler/utils/json_act.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 def salva_link_json(arquivo, novo_link):
 
@@ -26,7 +27,8 @@ def escolhe_spyder_json():
     print("| 2. Inativo                        |")
     print("| 3. Inativo                        |")
     print("| 4. Centauro                       |")
-    print("| 5. Declathon                      |")    
+    print("| 5. Declathon                      |")
+    print("| 6. sair                           |") 
     print("+-----------------------------------+")
 
     nome_arquivo = input("> ")
@@ -37,14 +39,18 @@ def escolhe_spyder_json():
         arquivo = 'centauro'
     elif nome_arquivo == '5':
         arquivo = 'declathon'
+    elif nome_arquivo == '6':
+        os.system("clear")
+        sys.exit(0)
     else:
         print("Escolha inválida, por favor, escolher 1, 4, ou 5")
         os.system('clear')
         escolhe_spyder_json()
 
-    arquivo = f"CrowCrawler/crowler/crowler/rules/{nome_arquivo}.json"
+    arquivo = f"crowler/rules/{arquivo}.json"
 
     novo_link = input("Insira o novo link: ")
 
-    salva_link_json(arquivo, novo_link)  
+    salva_link_json(arquivo, novo_link) 
+    os.system("clear")
 
