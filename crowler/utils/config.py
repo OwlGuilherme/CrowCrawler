8654ffa403c3f4ab137f59675278fe560c25bc1b
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-30 15:10:25 -0300

    Acrescentado site Magazine Luiza ao arquivo de configurações

--- crowler/utils/config.py
+++ crowler/utils/config.py
@@ -1,10 +1,11 @@
-from crowler.rules.rules import MercadoLivreRules, NetshoesRules, AmazonRules, CentauroRules, DeclathonRules
+from crowler.rules.rules import MercadoLivreRules, NetshoesRules, AmazonRules, CentauroRules, DeclathonRules, MagazineLuizaRules
 
 SITE_RULES = {
     'mercadolivre': MercadoLivreRules(),
     'netshoes': NetshoesRules(),
     'amazon' : AmazonRules(),
     'centauro' : CentauroRules(),
-    'declathon' : DeclathonRules()
+    'declathon' : DeclathonRules(),
+    'magazineluiza' : MagazineLuizaRules()
 }
 
