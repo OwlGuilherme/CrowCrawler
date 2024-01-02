f7ffcae155d5433dad4e4dea47c0e9d01549948d
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2024-01-02 15:54:30 -0300

    Alterada forma como a spider lida com o robots.txt

--- crowler/crowler/settings.py
+++ crowler/crowler/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "crowler.spiders"
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
